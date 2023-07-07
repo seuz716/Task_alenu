b7d39c67b522076d1a061d9f642d60e050f2becf
seuz716 <ceanabad@gmail.com>
2023-07-07 11:21:49 -0500

    ruta loging

--- alenu_servir/fundacion_alenu/urls.py
+++ alenu_servir/fundacion_alenu/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from tasks.views import signup, login, logout, home
+from tasks.views import signup, user_login, logout, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', signup, name='signup'),
-    path('login/', login, name='login'),
+    path('login/', user_login, name='login'),
     path('logout/', logout, name='logout'),
     path('', home, name='home'),
     # Otras rutas de la aplicación...
--- alenu_servir/tasks/views.py
+++ alenu_servir/tasks/views.py
@@ -8,14 +8,18 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LogoutView
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
-def login(request):
+def user_login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('home')
@@ -26,6 +30,8 @@ def login(request):
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
+
+
 
 
 class CustomLogoutView(LogoutView):
